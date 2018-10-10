d248b4fd6032f1bfe33d37151de2d9b197506146
Anders Hustvedt <anders@computerfire.net>
2018-10-09 20:49:16 -0600

    Bit array packing

--- Conway.cpp
+++ Conway.cpp
@@ -3,10 +3,10 @@
 
 namespace kaleidoscope {
 
-uint8_t Conway::board1[ROWS * 3][COLS];
-uint8_t Conway::board2[ROWS * 3][COLS];
-uint8_t (*Conway::board)[COLS] = board1;
-uint8_t (*Conway::otherBoard)[COLS] = board2;
+uint8_t Conway::board1[ROWS * 3 * COLS / 8];
+uint8_t Conway::board2[ROWS * 3 * COLS / 8];
+uint8_t *Conway::board = board1;
+uint8_t *Conway::otherBoard = board2;
 
 uint32_t Conway::next_heatmap_comp_time_ = 0;
 
@@ -19,8 +19,7 @@ void Conway::resetMap(void) {
   // getting special attention or if the user just wants a button to reset the map
   for (uint8_t r = 0; r < ROWS * 3; r++) {
     for (uint8_t c = 0; c < COLS; c++) {
-      board1[r][c] = 0;
-      board2[r][c] = 0;
+      set_value_at(r, c, 0, true);
     }
   }
 }
@@ -42,10 +41,9 @@ EventHandlerResult Conway::onKeyswitchEvent(Key &mapped_key, byte row, byte col,
   if (!keyToggledOn(key_state))
     return EventHandlerResult::OK;
 
-
-  board[row * 3][col] = 1;
-  board[row * 3 + 1][col] = 1;
-  board[row * 3 + 2][col] = 1;
+  set_value_at(row * 3, col, 1, true);
+  set_value_at(row * 3 + 1, col, 1, true);
+  set_value_at(row * 3 + 2, col, 1, true);
 
   return EventHandlerResult::OK;
 }
@@ -68,9 +66,10 @@ void Conway::update(void) {
   // for each key
   for (uint8_t r = 0; r < ROWS; r++) {
     for (uint8_t c = 0; c < COLS; c++) {
-      uint8_t red = board[r * 3][c];
-      uint8_t green = board[r * 3 + 1][c];
-      uint8_t blue = board[r * 3 + 2][c];
+
+      uint8_t red = get_value_at(r * 3, c);
+      uint8_t green = get_value_at(r * 3 + 1, c);
+      uint8_t blue = get_value_at(r * 3 + 2, c);
 
       // set the LED color accordingly
       ::LEDControl.setCrgbAt(r, c, computeColor(red, green, blue));
@@ -78,6 +77,29 @@ void Conway::update(void) {
   }
 
   stepBoard();
+}
+
+
+uint8_t Conway::get_value_at(uint8_t row, uint8_t column) {
+  uint8_t bit_index = row * COLS + column;
+
+  return (board[bit_index / 8] >> (bit_index % 8)) & 1;
+}
+uint8_t Conway::set_value_at(uint8_t row, uint8_t column, uint8_t value, bool front) {
+  uint8_t bit_index = row * COLS + column;
+
+  uint8_t *t;
+  if (front)
+    t = board;
+  else
+    t = otherBoard;
+  uint8_t b = t[bit_index / 8];
+  if (value == 1)
+    bitSet(b, bit_index % 8);
+  else
+    bitClear(b, bit_index % 8);
+  t[bit_index / 8] = b;
+
 }
 void Conway::stepBoard(void) {
   // Loop through every cell
@@ -90,7 +112,7 @@ void Conway::stepBoard(void) {
       for (int8_t i = -1; i <= 1; i++) {
         for (int8_t j = -1; j <= 1; j++) {
           if (i != 0 || j != 0) {
-            aliveNeighbours += board[(l + i + ROWS * 3) % (ROWS * 3)][(m + j + COLS) % COLS];
+            aliveNeighbours += get_value_at((l + i + ROWS * 3) % (ROWS * 3), (m + j + COLS) % COLS);
           }
         }
       }
@@ -98,22 +120,22 @@ void Conway::stepBoard(void) {
       // Implementing the Rules of Life
 
       // Cell is lonely and dies
-      uint8_t cell = board[l][m];
+      uint8_t cell = get_value_at(l, m);
       if ((aliveNeighbours < 2) || (aliveNeighbours > 3)) {
-        otherBoard[l][m] = 0;
+        set_value_at(l, m, 0, false);
       }
       // A new cell is born
       else if ((cell == 0) && (aliveNeighbours == 3)) {
-        otherBoard[l][m] = 1;
+        set_value_at(l, m, 1, false);
       }
       // Remains the same
       else {
-        otherBoard[l][m] = cell;
+        set_value_at(l, m, cell, false);
       }
     }
   }
 
-  uint8_t (*temp)[COLS] = board;
+  uint8_t *temp = board;
   board = otherBoard;
   otherBoard = temp;
 }
--- Conway.h
+++ Conway.h
@@ -20,12 +20,15 @@ class Conway : public LEDMode {
 
  private:
   static uint32_t next_heatmap_comp_time_;
-  static uint8_t board1[ROWS * 3][COLS];
-  static uint8_t board2[ROWS * 3][COLS];
-  static uint8_t (*board)[COLS];
-  static uint8_t (*otherBoard)[COLS];
+  static uint8_t board1[ROWS * 3 * COLS / 8];
+  static uint8_t board2[ROWS * 3 * COLS / 8];
+  static uint8_t *board;
+  static uint8_t *otherBoard;
   static cRGB computeColor(uint8_t r, uint8_t g, uint8_t b);
   static void stepBoard(void);
+  static uint8_t get_value_at(uint8_t row, uint8_t column);
+  static uint8_t set_value_at(uint8_t row, uint8_t column, uint8_t value, bool front);
+  
 };
 }
 
